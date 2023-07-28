ceacb91e9b08c79a58de961c9b20fb07881d9aa2
Ananth Narayan <narayanananth26@gmail.com>
2023-07-28 14:37:14 +0530

    build reusable styled comps

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-	background-color: orangered;
 	padding: 20px;
 `;
 
@@ -14,12 +14,31 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading as="h1">The Wild Oasis</Heading>
-				<Heading as="h2">Check in and out</Heading>
-				<Button onClick={() => {}}>Check in</Button>
-				<Button onClick={() => {}}>Check out</Button>
-				<Heading as="h3">Form world!</Heading>
-				<Input type="number" placeholder="Number of guests" />
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">The Wild Oasis</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button onClick={() => {}}>Check in</Button>
+							<Button variation="secondary" onClick={() => {}}>
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row type="vertical">
+						<Heading as="h3">Form world!</Heading>
+						<form>
+							<Input
+								type="number"
+								placeholder="Number of guests"
+							/>
+							<Input
+								type="number"
+								placeholder="Number of guests"
+							/>
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
