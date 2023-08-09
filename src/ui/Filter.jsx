1a252680ef9da10da8ff447df81b86d0c507a5cf
Ananth Narayan <narayanananth26@gmail.com>
2023-08-09 20:51:14 +0530

    api-side pagination: paginating bookings

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -16,7 +16,7 @@ const FilterButton = styled.button`
 	border: none;
 
 	${(props) =>
-		props.active &&
+		props.activated &&
 		css`
 			background-color: var(--color-brand-600);
 			color: var(--color-brand-50);
@@ -50,7 +50,7 @@ function Filter({ filterField, options }) {
 				<FilterButton
 					onClick={() => handleClick(option.value)}
 					key={option.value}
-					active={option.value === currentFilter}
+					activated={option.value === currentFilter}
 					disabled={option.value === currentFilter}
 				>
 					{option.label}
