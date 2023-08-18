5f3ae382fe89b65c8a6d67e998b33369c6ca38e1
Ananth Narayan <narayanananth26@gmail.com>
2023-08-18 15:20:48 +0530

    build dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	return (
+		<StyledDashboardLayout>
+			<div>Stats</div>
+			<div>Todays activity</div>
+			<div>chart stay duration</div>
+			<div>chart sales</div>
+		</StyledDashboardLayout>
+	);
+}
+
+export default DashboardLayout;
